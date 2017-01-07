2401754895af6a696669c568ef62c96676d192e9
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-06 19:11:28 -0500

    Add data to page with icon image

--- js/weather.js
+++ js/weather.js
@@ -11,4 +11,20 @@ getWeather(function(data) {
 	console.log('weather data received');
 	console.log(data.weather[0]);
 	console.log(data.main.temp);
+	console.log(data.name);
+
+	city = data.name;
+	temperature = data.main.temp;
+	conditionDescription = data.weather[0].description;
+	conditionIcon = data.weather[0].icon;
+	iconUrl = "http://openweathermap.org/img/w/" + conditionIcon + ".png";
+
+	console.log(conditionIcon);
+	console.log(iconUrl);
+
+	//Update webpage
+	document.getElementById("cityName").innerHTML = "Weather for: "+city;
+	document.getElementById("temp").innerHTML = "Temperature: "+temperature+ " K";
+	document.getElementById("condition").innerHTML = conditionDescription;
+	$("#icon").html("<img src='" + iconUrl + "'>");
 })
